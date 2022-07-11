1dc5a8051e70112d47f13d28749211d0740d377b
李小凯 <lixiaokai@live.com>
2022-07-11 17:15:54 +0800

    完善：服务提供定义配置

--- app/provider.php
+++ app/provider.php
@@ -1,9 +1,13 @@
 <?php
-use app\ExceptionHandle;
-use app\Request;
+declare (strict_types = 1);
 
-// 容器Provider定义文件
+/**
+ * 容器 Provider 定义文件.
+ *
+ * 说明：服务提供定义文件只能全局定义，不支持在应用下单独定义.
+ */
+
 return [
-    'think\Request'          => Request::class,
-    'think\exception\Handle' => ExceptionHandle::class,
+    think\Request::class            => app\Request::class,
+    think\exception\Handle::class   => app\ExceptionHandle::class,
 ];
