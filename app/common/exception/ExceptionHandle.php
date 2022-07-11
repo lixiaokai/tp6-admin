a5f29d0a91d35cdf251f87945927a1faf1ead17c
李小凯 <lixiaokai@live.com>
2022-07-11 17:57:20 +0800

    完善：服务提供定义和异常

---
+++ app/common/exception/ExceptionHandle.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\common\exception;
+
+use think\App;
+use think\exception\Handle;
+use think\Response;
+use Throwable;
+
+/**
+ * 应用异常处理类.
+ */
+class ExceptionHandle extends Handle
+{
+    /**
+     * 记录 - 异常信息 ( 包括日志或者其它方式记录 ).
+     */
+    public function report(Throwable $exception): void
+    {
+        // 使用内置的方式记录异常日志
+        parent::report($exception);
+    }
+
+    /**
+     * 处理 - 异常.
+     */
+    public function render($request, Throwable $e): Response
+    {
+        // 添加自定义异常处理机制
+
+        // 其他错误交给系统处理
+        return parent::render($request, $e);
+    }
+}
--- app/provider.php
+++ app/provider.php
@@ -3,11 +3,10 @@ declare (strict_types = 1);
 
 /**
  * 容器 Provider 定义文件.
- *
  * 说明：服务提供定义文件只能全局定义，不支持在应用下单独定义.
  */
 
 return [
-    think\Request::class            => app\Request::class,
-    think\exception\Handle::class   => app\ExceptionHandle::class,
+    think\Request::class            => app\common\request\Request::class,
+    think\exception\Handle::class   => app\common\exception\ExceptionHandle::class,
 ];
