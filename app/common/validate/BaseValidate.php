73f1c495c5dd4ad1da61c2616231a2a92ddedee1
李小凯 <lixiaokai@live.com>
2022-07-16 00:09:55 +0800

    完善：验证器

--- app/common/validate/BaseValidate.php
+++ app/common/validate/BaseValidate.php
@@ -1,20 +1,26 @@
 <?php
+declare(strict_types=1);
 
 namespace app\common\validate;
 
-use think\exception\ValidateException;
-use think\Request;
+use app\common\exception\ValidateException;
+use think\facade\Request;
 use think\Validate;
 
+/**
+ * 数据验证 - 基类.
+ */
 class BaseValidate extends Validate
 {
     /**
-     * 验证并获取验证后的数据.
+     * 获取 - 验证后的数据.
      */
-    public function checked(): array
+    public function validated(string $scene = null): array
     {
         $data = $this->getRequestData();
-        if (! $this->check($this->getRequestData())) {
+
+        $scene && $this->scene($scene);
+        if (! $this->check($data)) {
             throw new ValidateException($this->getError());
         }
 
@@ -22,10 +28,10 @@ class BaseValidate extends Validate
     }
 
     /**
-     * 获取 - 规则中包含字段的数据.
+     * 获取 - 规则中字段的请求数据.
      */
-    public function getRequestData(string $type = 'param'): array
+    public function getRequestData(): array
     {
-        return Request::instance()->only(array_keys($this->rule), $type);
+        return Request::param(array_keys($this->rule));
     }
 }
