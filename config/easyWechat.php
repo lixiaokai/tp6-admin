8ea00c0f7a1e11e27f4a2870b9771cd0928c7850
李小凯 <lixiaokai@live.com>
2022-07-19 15:57:47 +0800

    新增：jwt 和 easyWechat 包

---
+++ config/easyWechat.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * easyWechat 配置.
+ *
+ * 接口请求相关配置，超时时间等，具体可用参数请参考：
+ * @see https://github.com/symfony/symfony/blob/5.3/src/Symfony/Contracts/HttpClient/HttpClientInterface.php
+ */
+return [
+    'miniApp' => [
+        // 默认的小程序配置，也可以把 default 改成对应小程序标识
+        'default' => [
+            'app_id' => env('wechat.mini_app_appid'),
+            'secret' => env('wechat.mini_app_secret'),
+            'token' => env('wechat.mini_app_token'),
+            'aes_key' => env('wechat.mini_app_aes_key'),
+
+            'http' => [
+                'throw'  => true, // 状态码非 200、300 时是否抛出异常，默认为开启
+                'timeout' => 5.0,
+                // 'base_uri' => 'https://api.weixin.qq.com/', // 如果你在国外想要覆盖默认的 url 的时候才使用，根据不同的模块配置不同的 uri
+
+                'retry' => true, // 使用默认重试配置
+                //  'retry' => [
+                //      // 仅以下状态码重试
+                //      'http_codes' => [429, 500]
+                //       // 最大重试次数
+                //      'max_retries' => 3,
+                //      // 请求间隔 (毫秒)
+                //      'delay' => 1000,
+                //      // 如果设置，每次重试的等待时间都会增加这个系数
+                //      // (例如. 首次:1000ms; 第二次: 3 * 1000ms; etc.)
+                //      'multiplier' => 3
+                //  ],
+            ],
+        ]
+    ],
+];
